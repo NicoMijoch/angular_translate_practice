59e82ecbb1dee95c7b6cde980d41eef1d37115df
Nicolas Andres Mijoch <ZZJ019@laam.corp.gm.com>
2016-07-07 20:28:24 -0300

    filter y ui-sref-active

--- js/tanslationApp.js
+++ js/tanslationApp.js
@@ -8,15 +8,13 @@
 		  // Now set up the states
 		  $stateProvider
 			.state('state1', {
-			  url: "/state1",
+			  url: "/state1/:param1",
 			  templateUrl: "partials/state1.html"
 			})
 			.state('state1.list', {
 			  url: "/list",
 			  templateUrl: "partials/state1.list.html",
-			  controller: function($scope) {
-				$scope.items = ["A", "List", "Of", "Items"];
-			  }
+			  controller: "simpleCtrl"
 			})
 			.state('state2', {
 			  url: "/state2",
@@ -28,5 +26,26 @@
 			  controller: function($scope) {
 				$scope.things = ["A", "Set", "Of", "Things"];
 			  }
+			})
+			.state('state3', {
+			  url: "/state3",
+			  templateUrl: "partials/state3.html",
+			  controller: function($state){
+				  $state.go("state1", {param1: true});
+			  }
 			});
-		});
+		})
+		.controller("simpleCtrl", function($scope,$stateParams){
+			console.log($stateParams)
+			debugger;
+			$scope.demo="Simple Controller";
+		})
+		.filter('sacaras', function(){
+			return function(input) {
+				console.log(input);
+				return input.replace(/A/g,"");
+			}
+		})
+		.run(function($rootScope){
+          $rootScope.mensaje = "ANGULAR.JS";
+        });
